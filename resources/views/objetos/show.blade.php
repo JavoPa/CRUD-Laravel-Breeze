4696e4ce1358d85424babea645e9ad05d14cfcd8
Javier Placencia <javix.plax@gmail.com>
2023-12-22 22:07:42 -0300

    Margen de algunos botones

--- resources/views/objetos/show.blade.php
+++ resources/views/objetos/show.blade.php
@@ -10,16 +10,16 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 class="text-lg font-bold">{{ $objeto->nombre }}</h1>
-                    <p class="text-gray-600">{{ $objeto->descripcion }}</p>
+                    <p class="text-gray-600 mt-4">{{ $objeto->descripcion }}</p>
                     <a href="{{ route('objetos.edit', $objeto) }}">
-                        <x-secondary-button class="text-blue-500">
+                        <x-secondary-button class="text-blue-500 mt-4">
                             {{ __('Editar') }}
                         </x-secondary-button>
                     </a>
-                    <form method="POST" action="{{ route('objetos.destroy', $objeto) }}" class="inline">
+                    <form method="POST" action="{{ route('objetos.destroy', $objeto) }}" class="inline mt-4">
                         @csrf
                         @method('DELETE')
-                        <x-danger-button type="submit" class="ml-4">
+                        <x-danger-button type="submit">
                             {{ __('Eliminar') }}
                         </x-danger-button>
                     </form>
